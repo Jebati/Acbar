697d19e1a1d72db92ace7383599f6d71c36d9fff
Jebati <www.den-safonov@mail.ru>
2020-09-19 10:52:07 +0300

    Adding a display of information about drives

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Acbar
 {
@@ -6,7 +7,26 @@ namespace Acbar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine(new string('-', 32));
+
+                Console.WriteLine($"Name: {drive.Name}");
+                Console.WriteLine($"Drive type: {drive.DriveType}");
+                Console.WriteLine($"Fyle system: {drive.DriveFormat}");
+
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Disk volume: {drive.TotalSize}");
+                    Console.WriteLine($"Headspace: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Label: {drive.VolumeLabel}");
+                }
+
+                Console.WriteLine(new string('-', 32));
+                Console.WriteLine();
+            }
         }
     }
 }
